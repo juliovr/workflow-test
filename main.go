cc4430c2317e8a4c26a668ae88b56c0421a5226e
Julio Vidaurre <julio.vidaurre@gmail.com>
2020-08-28 15:52:17 -0400

    SSH ci.yml.

--- main.go
+++ main.go
@@ -11,5 +11,9 @@ import (
 func main() {
     fmt.Println("Init server")
     r := mux.NewRouter()
+
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "hello\n")
+    }).Methods(http.MethodGet)
     log.Fatal(http.ListenAndServe(":8003", r))
 }
